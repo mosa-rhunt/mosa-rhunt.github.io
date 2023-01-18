115033f88cf94ab854141d11423f05049814f001
Ryan <rhunt@mosaorganic.org>
2023-01-18 13:07:01 -0600

    change text print to print preview

--- enhanced_survey.js
+++ enhanced_survey.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
     .append("<span class='NoPrint' style='color:red; display:block; margin:20px 0;'><b>IMPORTANT:</b> Please click the Save Changes button below to save your entries before closing this window. You can return to complete or edit the OSP later.</span>")
     .append("<input type='button' value='Save Changes' class='menu' onclick='SaveForm()'>");
 
+    //change "Print" to "Print Preview"
+    $("a[href*='FormQuestionsPrint']").html("<img src='Images/Printer.gif'> Print Preview");
+
     //save button at bottom
     $("a.menu").each(function() {
         if ($(this).text().includes("Next")) {
