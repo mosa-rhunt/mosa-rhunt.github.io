d62fecb8cc56ac367f27b5eeb35a4e174256641f
Ryan <rhunt@mosaorganic.org>
2024-10-21 09:59:39 -0500

    comm events

--- new_client_onboarding.js
+++ new_client_onboarding.js
@@ -2,13 +2,19 @@ let html;
 let client_id;
 
 $(document).ready(function() {
+    $("head").append($.parseHTML(`
+    <style>
+
+    </style>
+    `));
+
+    // fetch data from print form
     client_id = $("#h_customerex").val();
     $.ajax({
         url: `PrintForm.asp?PrintFormNum=41&Action=View&CustomerAllNum=${client_id}`,
         type: "GET",
         success: function(response) {
-            html = $.parseHTML(response)
-            // go
+            html = $.parseHTML(response);
             specialties();
             osps();
             nco_info();
@@ -24,10 +30,15 @@ $(document).ready(function() {
             console.log(response);
         },
     });
+
+    // make the headers big
+    for (let id of [ "header-overview", "header-education", "header-staff-notes" ]) {
+        $(`#${id}`).css("color", "#333").css("font-size", "20pt");
+    }
 });
 
 
-function object_from_neworg_content(content_id, context=null) {
+function parse_neworg_content(content_id, context=null, as_array=true) {
     //this function uses the first column for the object keys, so its best to use an id for that column in the query
     let results = {};
     let headers = [];
@@ -52,24 +63,17 @@ function object_from_neworg_content(content_id, context=null) {
         }
     });
 
-    return results;
-}
-
-
-function array_from_neworg_content(content_id, context=null) {
-    return Object.values(object_from_neworg_content(content_id, context));
+    return (as_array ? Object.values(results) : results);
 }
 
 
 function specialties() {
-    let root = $("#specialties");
-    let parsed = object_from_neworg_content("315", html);
-    let specialties = [];
-    for (let key in Object.values(parsed)[0]) {
-        if (key == "CustomerNum") continue;
-        if (parsed[key] == "Yes") specialties.push(key);
+    let parsed = parse_neworg_content("315", html)[0];
+    let list = [];
+    for (let key in parsed) {
+        if (parsed[key] == "Yes") list.push(key);
     }
-    $(root).text(specialties.join(", "));
+    $("#specialties").text(list.join(", "));
 }
 
 
@@ -110,11 +114,26 @@ function compliance_topics() {
 
 
 function comm_events() {
-    let root = $("#comm-events");
-    let parsed = object_from_neworg_content("1822", html);
-    for (let event of Object.values(parsed)) {
-        $(root).append(event.Name).append("<br>");
+    let table = $("<table></table>");
+    let thead = $("<thead class='thead-light'></thead>").appendTo(table);
+    let tbody = $("<tbody></tbody>").appendTo(table);
+
+    $("<tr></tr>")
+    .append("<th>Event Date</th>")
+    .append("<th>Topic (Name)</th>")
+    .append("<th>Minutes</th>")
+    .appendTo(thead);
+
+    let parsed = parse_neworg_content("1822", html);
+    for (let event of parsed) {
+        $("<tr></tr>")
+        .append(`<td><a href="https://web5.neworg.com/MC2_MOSA/EventUpdate.asp?Action=View&EventNum=${event.EventNum}" target="_blank">${event.EventDate}</a></td>`)
+        .append(`<td>${event.Name}</td>`)
+        .append(`<td>${event.Minutes}</td>`)
+        .appendTo(tbody);
     }
+
+    $("#comm-events").append(table);
 }
 
 
